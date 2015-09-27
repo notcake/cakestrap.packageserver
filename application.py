fa23e7ce8c09009c92cd7ee8652be1311c2965da
notcake <cakenotfound@gmail.com>
2015-09-27 19:29:00 +0100

    Added debug mode support.

--- application.py
+++ application.py
@@ -1,9 +1,22 @@
+import flask
 from flask import Flask
+
+from session import Session
+
 app = Flask(__name__)
+
+# Blueprints
+from loginblueprint import blueprint as loginBlueprint
+app.register_blueprint(loginBlueprint)
 
 @app.route("/")
 def index():
-	return "CakeStrap Package Server"
+	session = Session(flask.session)
+	
+	return flask.render_template("index.html")
 
 if __name__ == "__main__":
-	app.run ()
+	print ("Starting server in debug mode...")
+	
+	app.debug = True
+	app.run (host = "0.0.0.0")
