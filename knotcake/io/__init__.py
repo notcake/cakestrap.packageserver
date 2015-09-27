4d9b53b85cf498938d8ee9ed94afa0331287ebc9
notcake <cakenotfound@gmail.com>
2015-09-27 18:55:02 +0100

    Added skeleton for Knotcake.IO.

---
+++ knotcake/io/__init__.py
@@ -0,0 +1,16 @@
+from ibasestream import IBaseStream
+from iinstream import IInStream
+from ioutstream import IOutStream
+
+from endianness import Endianness
+from istreamreader import IStreamReader
+from istreamwriter import IStreamWriter
+
+from streamreader import StreamReader
+from streamwriter import StreamWriter
+
+from bufferedstreamreader import BufferedStreamReader
+from bufferedstreamwriter import BufferedStreamWriter
+
+from stringinstream import StringInStream
+from stringoutstream import StringOutStream
