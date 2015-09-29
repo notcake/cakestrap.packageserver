5286b3fcb31a32c037782ce10552e71a475e631b
notcake <cakenotfound@gmail.com>
2015-09-29 01:13:57 +0100

    Fixed Knotcake.Steam.OpenId

--- knotcake/steam/openid.py
+++ knotcake/steam/openid.py
@@ -2,14 +2,15 @@ import urllib
 
 class OpenId(object):
 	def __init__(self):
-		self.localUrl = None
+		self.localIdentity = None
+		self.returnUrl     = None
 	
 	def generateLoginUrl(self):
 		parameters = {
 			"openid.ns":         "http://specs.openid.net/auth/2.0",
 			"openid.mode":       "checkid_setup",
-			"openid.return_to":  "true",
-			"openid.realm":      self.localUrl,
+			"openid.return_to":  self.returnUrl,
+			"openid.realm":      self.localIdentity,
 			"openid.identity":   "http://specs.openid.net/auth/2.0/identifier_select",
 			"openid.claimed_id": "http://specs.openid.net/auth/2.0/identifier_select"
 		}
@@ -17,4 +18,3 @@ class OpenId(object):
 		parameters = [ k + "=" + v for k, v in parameters.iteritems() ]
 		parameters = str.join("&", parameters)
 		return "https://steamcommunity.com/openid/login?" + parameters
-	
