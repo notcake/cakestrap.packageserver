5fbb8c98a15e27e11fca0f42a7e2fceb26a7753e
notcake <cakenotfound@gmail.com>
2015-10-01 23:22:14 +0100

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
+sudo apt-get install libmysqlclient-dev
+
 virtualenv venv
 . venv/bin/activate
 	pip install flask
 	pip install sqlalchemy
 	pip install urllib3
 	pip install certifi
+	pip install mysql-python
 deactivate
 
