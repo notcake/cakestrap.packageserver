7dd0229b93cdda4c3cda5f7b6193f7a74ff1cf17
notcake <cakenotfound@gmail.com>
2015-10-02 01:30:57 +0100

    Updated config.example.py

--- config.example.py
+++ config.example.py
@@ -2,7 +2,8 @@ config = {
 	"Database": {
 		"Hostname": "localhost",
 		"Username": "root",
-		"Password": "password"
+		"Password": "password",
+		"DatabaseName": "cakestrap.packageserver"
 	},
 	"SteamApiKey": "0123456789ABCDEF0123456789ABCDEF"
 }
