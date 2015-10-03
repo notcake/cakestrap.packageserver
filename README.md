b6cf48efe441bfc0715a51b6964f31c408cccb28
notcake <cakenotfound@gmail.com>
2015-10-03 18:01:07 +0100

    Updated README.sh and setup.sh to support PyReact

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 You will need to install:
 * libapache2-mod-wsgi
 * virtualenv
+* gcc
+* libmysqlclient-dev
+* nodejs
 
 Alternatively do whatever you want to get it running.
-
--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
+sudo apt-get install libapache2-mod-wsgi
+sudo apt-get install virtualenv
+sudo apt-get install gcc
+
 sudo apt-get install libmysqlclient-dev
+sudo apt-get install nodejs
 
 virtualenv venv
 . venv/bin/activate
@@ -7,5 +12,8 @@ virtualenv venv
 	pip install urllib3
 	pip install certifi
 	pip install mysql-python
+	pip install pyreact
 deactivate
 
+mkdir -p data
+chown -R :www-data data
