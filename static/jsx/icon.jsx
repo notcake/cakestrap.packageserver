c24d52fc6c522c68e78d098e1c21f24766420d77
notcake <cakenotfound@gmail.com>
2015-10-24 02:35:05 +0100

    Added support for FontAwesome icons to Icon.

--- static/jsx/icon.jsx
+++ static/jsx/icon.jsx
@@ -13,6 +13,10 @@ var Icon = React.createClass(
 			{
 				icon = "/static/images/silkicons/" + icon + ".png";
 			}
+			else if (/^fa:[a-zA-Z0-9_]*$/.test(icon))
+			{
+				return <span className={ "fa fa-" + icon.substr(3) } style={ { verticalAlign: "middle", color: this.props.color } }/>;
+			}
 			
 			return <img src={ icon } style={ { verticalAlign: "middle" } } />;
 		}
