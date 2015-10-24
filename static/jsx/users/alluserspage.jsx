b355723aa62a3f440b7567e867b18af0232ad088
notcake <cakenotfound@gmail.com>
2015-10-24 02:54:21 +0100

    Made AllUsersPage use the GridListView component.

--- static/jsx/users/alluserspage.jsx
+++ static/jsx/users/alluserspage.jsx
@@ -6,45 +6,7 @@ var AllUsersPage = React.createClass(
 				<div style={ { width: "50%", margin: "8px auto" } }>
 					<h2>Users</h2>
 					<hr />
-					<div style={ { textAlign: "center" } }>
-						<div style={ { display: "inline", textAlign: "left" } }>
-							{
-								this.props.users.map(
-									function(user)
-									{
-										return (
-											<div key={ user.id } style={ { display: "inline-block", width: "30%", margin: "8px 8px", backgroundColor: "beige" } }>
-												<a className="block" href={ "/users/" + user.steamId64 } style={ { display: "block", overflow: "auto", width: "100%", padding: "8px" } }>
-													<div className="col-md-4" style={ { width: "auto", marginRight: "8px" } }>
-														<img src={ user.mediumProfilePictureUrl } style={ { width: "48px", height: "48px" } } />
-													</div>
-													<div style={ { float: "left", margin: "0px 0px" } }>
-														<span style={ { fontSize: "110%" } }>{ user.displayName }</span>
-														<br />
-														<span style={ { color: "gray" } }>
-															(<img src={ user.getRankIcon() } style={ { verticalAlign: "top", margin: "0px 2px" } } />{ user.getRankName() })
-														</span>
-													</div>
-												</a>
-											</div>
-										);
-									}.bind(this)
-								)
-							}
-							{
-								this.props.users.length % 3 != 0 &&
-								this.props.users.length % 3 <= 1 ?
-									<div style={ { display: "inline-block", width: "30%", margin: "8px 8px" } } /> :
-									null
-							}
-							{
-								this.props.users.length % 3 != 0 &&
-								this.props.users.length % 3 <= 2 ?
-									<div style={ { display: "inline-block", width: "30%", margin: "8px 8px" } } /> :
-									null
-							}
-						</div>
-					</div>
+					<GridListView viewMode="grid" itemClass={ UserGridListViewItem } items={ this.props.users } />
 				</div>
 			);
 		}
