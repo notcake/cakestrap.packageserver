a1f915fc8a1741f5759c78e502d6c7a80a16f8db
notcake <cakenotfound@gmail.com>
2015-10-24 02:34:41 +0100

    Added pressed attribute to Button.

--- static/jsx/button.jsx
+++ static/jsx/button.jsx
@@ -3,17 +3,13 @@ var Button = React.createClass(
 		render: function()
 		{
 			return (
-				<a href={ this.props.href || "#" } className="btn btn-default" onClick={ this.handleClick }>
+				<a href={ this.props.href || "#" } className={ "btn btn-default" + (this.props.pressed ? " active" : "") } onClick={ this.handleClick }>
 					{
 						this.props.icon ?
-							<Icon icon={ this.props.icon } style={ { verticalAlign: "middle", marginRight: "4px" } } />:
+							<Icon icon={ this.props.icon } style={ { verticalAlign: "middle", marginRight: this.props.text ? "4px" : "0px" } } />:
 							null
 					}
-					{
-						this.props.text ?
-							<span style={ { verticalAlign: "middle" } }>{ this.props.text }</span> :
-							null
-					}
+					<span style={ { verticalAlign: "middle" } }>{ this.props.text }</span>
 					{ this.props.children }
 				</a>
 			);
