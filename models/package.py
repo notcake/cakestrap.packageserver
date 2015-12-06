7b2b12b277d22d7ab07bb901ca2e302adf91b7d4
notcake <cakenotfound@gmail.com>
2015-12-06 17:43:42 +0000

    Updated models.

--- models/package.py
+++ models/package.py
@@ -2,7 +2,7 @@ from sqlalchemy import select, func
 from sqlalchemy import Column, PrimaryKeyConstraint, ForeignKey, UniqueConstraint
 from sqlalchemy import Boolean, Integer, BigInteger, Text
 from sqlalchemy import Enum
-from sqlalchemy.orm import relationship, column_property
+from sqlalchemy.orm import relationship, column_property, reconstructor
 
 from base import Base
 
--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from sqlalchemy import select, func
 from sqlalchemy import Column, PrimaryKeyConstraint, ForeignKey, UniqueConstraint
 from sqlalchemy import Boolean, Integer, BigInteger, Text
 from sqlalchemy import Enum
-from sqlalchemy.orm import relationship, column_property
+from sqlalchemy.orm import relationship, column_property, reconstructor
 
 from base import Base
 from steamuser import SteamUser
@@ -29,6 +29,10 @@ class User(Base):
 	PrimaryKeyConstraint(id)
 	
 	def __init__(self):
+		self.init()
+	
+	@reconstructor
+	def init(self):
 		self.rank = "user"
 	
 	def isAnonymous(self):
