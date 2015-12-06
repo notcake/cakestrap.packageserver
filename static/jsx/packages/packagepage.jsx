0d19adc15a9ac06e64cc3f4cc2858cac6bf2a889
notcake <cakenotfound@gmail.com>
2015-12-06 17:23:51 +0000

    Fixed bad layout when viewing an invalid package.

--- static/jsx/packages/packagepage.jsx
+++ static/jsx/packages/packagepage.jsx
@@ -8,6 +8,7 @@ var PackagePage = React.createClass(
 						<Button visible={ currentUser.canEditPackage(this.props.package) } href={ "/packages/" + this.props.package.id + "/edit" } icon="pencil" text="Edit" />
 					</div>
 					<h2>{ this.props.package.displayName }</h2>
+					<div style={ { clear: "both" } } />
 					<hr />
 					<div style={ { overflow: "auto", width: "100%", margin: "8px auto", padding: "16px", backgroundColor: "beige" } }>
 						<div>
