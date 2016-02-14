2789d357163fd5c1543426c64540308e9d7e520b
notcake <cakenotfound@gmail.com>
2016-02-14 15:43:28 +0000

    Updated Package model.

--- static/js/models/package.js
+++ static/js/models/package.js
@@ -3,14 +3,31 @@ var Package = Knotcake.OOP.Class(self)
 
 self.ctor = function(packageInformation)
 {
-	this.id            = null;
-	this.name          = null;
-	this.displayName   = null;
-	this.description   = null;
-	this.creatorUserId = null;
+	this.id                       = null;
+	this.name                     = null;
+	this.displayName              = null;
+	this.description              = null;
+	this.creatorUserId            = null;
+	this.codeDirectoryTreeId      = null;
+	this.resourcesDirectoryTreeId = null;
+	
+	this.codeDirectoryTree        = null;
+	this.resourcesDirectoryTree   = null;
 	
 	for (var k in packageInformation)
 	{
 		this[k] = packageInformation[k];
 	}
+	
+	if (this.codeDirectoryTree) {
+		this.codeDirectoryTree = new DirectoryTree(this.codeDirectoryTree);
+	}
+	if (this.resourcesDirectoryTree) {
+		this.resourcesDirectoryTree = new DirectoryTree(this.resourcesDirectoryTree);
+	}
+};
+
+self.getBasePath = function()
+{
+	return "/packages/" + this.id;
 };
